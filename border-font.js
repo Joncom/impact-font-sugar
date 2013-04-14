7539e0444e8f792b304a4b3dfde04d000c7894b2
Jonathan Commins <joncom@gmail.com>
2013-04-13 23:15:07 -0700

    Created _getNonAlphaPixels method.

--- border-font.js
+++ border-font.js
@@ -9,16 +9,29 @@ ig.module('plugins.joncom.border-font')
         borderSize: 1,
 
         onLoad: function(event) {
-            this._addBorderToFont();
+            this._addBorderToFont(this.data);
             this.parent(event);
         },
 
-        _addBorderToFont: function() {
-            // First create an offscreen canvas.
+        _getNonAlphaPixels: function(image) {
+            // Create an offscreen canvas.
             var canvas = ig.$new('canvas');
-            // Add extra space for border.
-            canvas.width = this.data.width + (this.borderSize * 2);
-            canvas.height = this.data.height + (this.borderSize * 2);
+            canvas.width = image.width;
+            canvas.height = image.height;
+            // Draw image to canvas.
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0);
+            // Determine which pixels are alpha and which are not.
+            var data = ctx.getImageData(0, 0, image.width, image.height);
+            var nonAlphaPixels = [];
+            for(var x = 0; x < canvas.width; x++) {
+                if(typeof nonAlphaPixels[x] === 'undefined') nonAlphaPixels[x] = [];
+                for(var y = 0; y < canvas.height; y++) {
+                    var alpha = data[((image.width * y) + x) * 4 + 3]; // alpha data for pixel
+                    nonAlphaPixels[x][y] = (alpha !== 0);
+                }
+            }
+            return nonAlphaPixels;
         }
 
     });
